fd8fc5dcf1f7dd00ac3107f89a4d8b24212ee0ca
SerkayKarakaya <serkay1karakaya@gmail.com>
2025-09-15 11:50:25 +0300

    ....

---
+++ FinancailCrm/frmLogin.cs
@@ -0,0 +1,51 @@
+﻿using FinancailCrm.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinancailCrm
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        FinancialCrmDbEntities db = new FinancialCrmDbEntities();
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var username = txtUser.Text;
+            var pass = txtPassword.Text;
+
+            var user = db.Users.FirstOrDefault(x => x.UserName == username);
+
+            if (user != null && user.Password == pass)
+            {
+                if (user.Password == pass)
+                {
+                    MessageBox.Show("Giriş Başarılı");
+                    this.Hide();
+                    frmDashboard dashboard = new frmDashboard();
+                    dashboard.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Password is incorrect");
+                }
+            }
+            else
+            {
+                MessageBox.Show("User not found");
+
+            }
+        }
+    }
+}
